5d6cd625dc61a8afe3fc484511b132fdc6939f9f
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-05-11 12:17:23 -0400

    added solution 00261

---
+++ solutions/medium/00261-Graph-Valid-Tree/solution.cpp
@@ -0,0 +1,61 @@
+class DisjointSets {
+private:
+    vector<int> rank;
+    vector<int> parent;
+    int total_roots;
+
+public:
+    DisjointSets(int size) {
+        this->rank = vector(size, 1);
+        this->parent.reserve(size);
+        this->total_roots = size;
+
+        for (int i = 0; i < size; i++)
+            this->parent.push_back(i);
+    }
+
+    int findRoot(int target) {
+        if (target == parent[target])
+            return target;
+
+        return parent[target] = findRoot(parent[target]);
+    }
+
+    void makeUnion(int x, int y) {
+        int x_root = findRoot(x);
+        int y_root = findRoot(y);
+
+        if (x_root == y_root)
+            return;
+
+        if (rank[x_root] > rank[y_root])
+            parent[y_root] = x_root;
+        else if (rank[x_root] < rank[y_root])
+            parent[x_root] = y_root;
+        else {
+            parent[y_root] = x_root;
+            rank[x_root] += 1;
+        }
+
+        this->total_roots--;
+    }
+
+    int totalRoots() {
+        return this->total_roots;
+    }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>> &edges) {
+        DisjointSets disjoint_sets = DisjointSets(n);
+
+        for (int i = 0; i < edges.size(); i++)
+            if (disjoint_sets.findRoot(edges[i][0]) != disjoint_sets.findRoot(edges[i][1]))
+                disjoint_sets.makeUnion(edges[i][0], edges[i][1]);
+            else
+                return false;
+
+        return disjoint_sets.totalRoots() == 1;
+    }
+};
