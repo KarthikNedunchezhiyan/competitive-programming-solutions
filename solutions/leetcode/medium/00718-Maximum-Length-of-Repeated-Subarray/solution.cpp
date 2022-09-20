eec2e521d384eb18b1777b33957a1f68144fc20d
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-09-20 10:03:28 -0400

    updated solution 00718

--- solutions/leetcode/medium/00718-Maximum-Length-of-Repeated-Subarray/solution.cpp
+++ solutions/leetcode/medium/00718-Maximum-Length-of-Repeated-Subarray/solution.cpp
@@ -1,26 +1,12 @@
 class Solution {
-private:
-    int r, c;
-    vector<vector<int>> memo;
-
-    int get_value(int i, int j) {
-        return (i < 0 || j < 0 || i >= r || j >= c) ? 0 : memo[i][j];
-    }
-
 public:
     int findLength(vector<int> &nums1, vector<int> &nums2) {
-        this->r = nums1.size();
-        this->c = nums2.size();
-        this->memo = vector(nums1.size(), vector(nums2.size(), 0));
-
+        int dp[nums1.size()][nums2.size()];
         int result = 0;
-        for (int i = 0; i < r; i++)
-            for (int j = 0; j < c; j++) {
-                if (nums1[i] == nums2[j])
-                    memo[i][j] = get_value(i - 1, j - 1) + 1;
-
-                result = max(result, memo[i][j]);
-            }
+        for (int i = 0; i < nums1.size(); ++i)
+            for (int j = 0; j < nums2.size(); ++j)
+                result = max(result,
+                             dp[i][j] = nums1[i] != nums2[j] ? 0 : i && j ? dp[i - 1][j - 1] + 1 : 1);
 
         return result;
     }
