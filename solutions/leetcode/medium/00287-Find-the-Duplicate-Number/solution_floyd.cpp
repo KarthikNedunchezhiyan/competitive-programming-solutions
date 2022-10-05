e45692e0a0849d21b5521a41a1f41f93c94a8320
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-10-05 09:04:24 -0400

    added solution 00287

---
+++ solutions/leetcode/medium/00287-Find-the-Duplicate-Number/solution_floyd.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int> &nums) {
+        int tortoise = 0, hare = 0;
+        do {
+            hare = nums[nums[hare]];
+            tortoise = nums[tortoise];
+        } while (tortoise != hare);
+        
+        
+        tortoise = 0;
+        while (tortoise != hare) {
+            hare = nums[hare];
+            tortoise = nums[tortoise];
+        } 
+
+        return hare;
+    }
+};
