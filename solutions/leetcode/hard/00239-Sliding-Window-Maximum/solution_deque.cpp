cfc31028c88f08e994c7f10f086dcbba44ceb4dd
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-10-21 15:03:14 -0400

    added solution 00239

--- solutions/leetcode/hard/00239-Sliding-Window-Maximum/solution_deque.cpp
+++ solutions/leetcode/hard/00239-Sliding-Window-Maximum/solution_deque.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int> &nums, int k) {
-        deque<int> monotonic_dec;
+        deque<int> window;
         vector<int> result;
-        result.reserve(nums.size() - k + 1);
 
         for (int i = 0; i < nums.size(); ++i) {
-            // removing the outdated from back
-            while (!monotonic_dec.empty() && monotonic_dec.front() <= (i - k)) monotonic_dec.pop_front();
-            // removing the non-participants from front
-            while (!monotonic_dec.empty() && nums[monotonic_dec.back()] <= nums[i]) monotonic_dec.pop_back();
+            while (!window.empty() && window.front() <= (i - k)) window.pop_front();
+            while (!window.empty() && nums[window.back()] < nums[i]) window.pop_back();
+            window.emplace_back(i);
 
-            monotonic_dec.emplace_back(i);
-            if (i >= k - 1) result.emplace_back(nums[monotonic_dec.front()]);
+            if (i >= k - 1) result.emplace_back(nums[window.front()]);
         }
 
         return result;
