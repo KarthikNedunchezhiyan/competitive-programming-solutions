bba27b0f6fe47ac3850c5d832ab4c29fa9c84458
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-10-22 18:54:50 -0400

    added solution 00121

--- solutions/leetcode/easy/00121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
+++ solutions/leetcode/easy/00121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
@@ -1,25 +1,12 @@
 class Solution {
-private:
-    vector<int> prices;
-    vector<vector<int>> memo;
-
-    int f(int pi, bool buy) {
-        if (pi >= prices.size())
-            return 0;
-
-        if (memo[pi][buy] != INT_MIN)
-            return memo[pi][buy];
-
-        if (buy)
-            return memo[pi][buy] = max(f(pi + 1, buy), f(pi + 1, !buy) - prices[pi]);
-
-        return memo[pi][buy] = max(f(pi + 1, buy), prices[pi]);
-    }
-
 public:
     int maxProfit(vector<int> &prices) {
-        this->prices = prices;
-        this->memo = vector(prices.size(), vector(2, INT_MIN));
-        return f(0, true);
+        int max_val = prices.back(), profit = 0;
+        for (int i = prices.size() - 2; i >= 0; --i) {
+            max_val = max(max_val, prices[i]);
+            if (prices[i] < max_val && (max_val - prices[i]) > profit) profit = max_val - prices[i];
+        }
+
+        return profit;
     }
 };
