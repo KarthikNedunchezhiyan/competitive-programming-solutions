be03ee3bba4309d18839643a615f852596a76583
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-12-11 23:37:37 -0500

    added solution 01578

--- solutions/leetcode/medium/01578-Minimum-Time-to-Make-Rope-Colorful/solution.cpp
+++ solutions/leetcode/medium/01578-Minimum-Time-to-Make-Rope-Colorful/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int minCost(string colors, vector<int> &neededTime) {
-        int i = 0, j = 0, total_time = 0, max_time = 0, result = 0;
-
+        int total_time = 0, max_time = 0, i = 0, j = 0, result = 0;
         while (j < colors.size()) {
             if (colors[i] == colors[j]) {
                 total_time += neededTime[j];
                 max_time = max(max_time, neededTime[j]);
             } else {
-                if (i != j - 1)
-                    result += total_time - max_time;
+                if (i != j - 1) result += total_time - max_time;
                 i = j;
                 total_time = max_time = neededTime[j];
             }
-            j++;
+            ++j;
         }
-        
-        return (i != j - 1) ? result + total_time - max_time : result;
+
+        return (i == j - 1) ? result : result + total_time - max_time;
     }
 };
