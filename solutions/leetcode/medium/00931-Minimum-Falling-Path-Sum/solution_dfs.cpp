bb768f59bf6a4fcb3ba99e75b2259044385591a3
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2022-12-12 21:54:56 -0500

    added solution 00931

--- solutions/leetcode/medium/00931-Minimum-Falling-Path-Sum/solution_dfs.cpp
+++ solutions/leetcode/medium/00931-Minimum-Falling-Path-Sum/solution_dfs.cpp
@@ -1,32 +1,30 @@
 class Solution {
 private:
-    vector<vector<int>> matrix;
     int m, n;
     vector<vector<int>> memo;
 
-    int f(int i, int j) {
+    int f(int i, int j, vector<vector<int>> &matrix) {
         if (i >= m || j >= n || i < 0 || j < 0)
             return INT_MAX;
 
         if (memo[i][j] != -1)
             return memo[i][j];
 
-        int further_moves = min({f(i + 1, j - 1), f(i + 1, j), f(i + 1, j + 1)});
+        int further_moves = min({f(i + 1, j - 1, matrix), f(i + 1, j, matrix), f(i + 1, j + 1, matrix)});
         further_moves = further_moves == INT_MAX ? 0 : further_moves;
         return memo[i][j] = matrix[i][j] + further_moves;
     }
 
 public:
     int minFallingPathSum(vector<vector<int>> &matrix) {
-        this->matrix = matrix;
         this->m = matrix.size();
         this->n = matrix[0].size();
         this->memo = vector(m, vector(n, -1));
 
         int result = INT_MAX;
         for (int i = 0; i < n; i++)
-            result = min(result, f(0, i));
-        
+            result = min(result, f(0, i, matrix));
+
         return result;
     }
 };
